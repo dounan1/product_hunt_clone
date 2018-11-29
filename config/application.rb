88f7043a35de5e9464760842ccc7545afb6eb2ba
Dounan Hu <dounan1@gmail.com>
2018-11-30 04:24:49 +0800

    running

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,14 @@ module ProductHuntClone
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+
+    Capybara.register_driver :headless_chrome do |app|
+      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+        'chromeOptions' => { args: %w(headless disable-gpu) + [ 'window-size=1280,800' ] })
+      Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
+    end
+    Capybara.save_path = Rails.root.join('tmp/capybara')
+    Capybara.javascript_driver = :headless_chrome
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
